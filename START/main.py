83b44f349f1258c66db10b3a8a37058b06882b43
JinKyeong Kim <jinkeung1203@gmail.com>
2024-06-18 22:28:41 +0900

    0618_2

--- START/main.py
+++ START/main.py
@@ -1,8 +1,16 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+import pandas as pd
 
-if __name__ == '__main__':
-    print("Sleeeeeeeeeeeeepy")
-    print("222")
-    print("333")
-    print("babo")
-    print("you are babo")
+iris=load_iris()
+data=iris.data
+field=iris.feature_names
+index=iris.target
 
+df=pd.DataFrame(data,index,field)
+print(df)
+
+for i in range(0,3):
+    plt.scatter(data[index==i,0],data[index==i,1])
+plt.show()
